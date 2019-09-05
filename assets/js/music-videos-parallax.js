8869ae7b33708f667b7a3fd8cbbbd9e2d81c739b
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-09-05 14:35:35 +0200

    add leaf2 parallax

--- assets/js/music-videos-parallax.js
+++ assets/js/music-videos-parallax.js
@@ -17,3 +17,45 @@ function elementDistanceFromBottomOfViewport(el) {
 
   return window.innerHeight - rect.top;
 }
+
+function parallaxLeif1() {
+  let container = document.getElementById("music-videos-leaf-1-box");
+  console.log(container);
+  let multiplier = 0.4;
+  let distance = elementDistanceFromBottomOfViewport(container);
+
+  container.style.transform = "translateY(-" + distance * multiplier + "px)";
+}
+
+if (window.addEventListener) {
+  addEventListener('DOMContentLoaded', parallaxLeif1, false);
+  addEventListener('load', parallaxLeif1, false);
+  addEventListener('scroll', parallaxLeif1, false);
+}
+
+function elementDistanceFromBottomOfViewport(el) {
+  let rect = el.getBoundingClientRect();
+
+  return window.innerHeight - rect.top;
+}
+
+function parallaxLeif2() {
+  let container = document.getElementById("music-videos-leaf-2-box");
+  console.log(container);
+  let multiplier = 0.4;
+  let distance = elementDistanceFromBottomOfViewport(container);
+
+  container.style.transform = "translateY(" + distance * multiplier + "px)";
+}
+
+if (window.addEventListener) {
+  addEventListener('DOMContentLoaded', parallaxLeif2, false);
+  addEventListener('load', parallaxLeif2, false);
+  addEventListener('scroll', parallaxLeif2, false);
+}
+
+function elementDistanceFromBottomOfViewport(el) {
+  let rect = el.getBoundingClientRect();
+
+  return window.innerHeight - rect.top;
+}
