44fbb300843059fab619697af64bc4857985ca23
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-09-07 09:07:57 +0200

    create music, videos and store scss files

--- assets/js/music-videos-hover.js
+++ assets/js/music-videos-hover.js
@@ -1,22 +1,22 @@
 // Submit button animation
 
-let submitButtonMouseover = document.querySelector(".music-videos-discography-item");
-let submitButtonMouseout = document.querySelector(".music-videos-discography-item");
+// let submitButtonMouseover = document.querySelector(".music-videos-discography-item");
+// let submitButtonMouseout = document.querySelector(".music-videos-discography-item");
 
-submitButtonMouseover.addEventListener("mouseover", function () {
-  anime({
-    targets: ".music-videos-discography-item",
-    scale: 1.3,
-    easing: "easeInCubic",
-    duration: 400,
-  });
-});
+// submitButtonMouseover.addEventListener("mouseover", function () {
+//   anime({
+//     targets: ".music-videos-discography-item",
+//     scale: 1.3,
+//     easing: "easeInCubic",
+//     duration: 400,
+//   });
+// });
 
-submitButtonMouseout.addEventListener("mouseout", function () {
-  anime({
-    targets: ".music-videos-discography-item",
-    scale: 1,
-    easing: "easeInCubic",
-    duration: 400,
-  });
-});
+// submitButtonMouseout.addEventListener("mouseout", function () {
+//   anime({
+//     targets: ".music-videos-discography-item",
+//     scale: 1,
+//     easing: "easeInCubic",
+//     duration: 400,
+//   });
+// });
