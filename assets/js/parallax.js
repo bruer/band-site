2a9af293cd74fc302d5436ae9bf6d809f1d2e02e
“MikaelaJ” <Mikaela_johansson@hotmail.com>
2019-09-10 10:48:14 +0200

    fantastic parallax a la fredrik och valesca

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,119 +1,22 @@
-if (window.addEventListener) {
-  addEventListener("DOMContentLoaded", parallaxLeaf, false);
-  addEventListener("load", parallaxLeaf, false);
-  addEventListener("scroll", parallaxLeaf, false);
+function parallax() {
+  let el = document.querySelectorAll(".parallax")
+  for (let i = 0; i < el.length; i++) {
+    let direction = el[i].getAttribute("data-direction");
+    let mult = el[i].getAttribute("data-mult");
+    let distance = elementDistanceFromBottomOfViewport(el[i]);
+    /* el[i].style.transform = `translate ${ + direction + distance * mult} px)`; */
+    el[i].style.transform = "translate" + direction + distance * mult + "px)";
+  }
 }
 
+if (window.addEventListener) {
+  addEventListener('DOMContentLoaded', parallax, false);
+  addEventListener('load', parallax, false);
+  addEventListener('scroll', parallax, false);
+}
+
+// DistanceFromBottom
 function elementDistanceFromBottomOfViewport(el) {
   let rect = el.getBoundingClientRect();
   return window.innerHeight - rect.top;
-}
-
-/* TEST - funkar ej */
-/* function parallaxTest(attribute) {
-  let el = document.querySelectorAll(".parallaxTest");
-  let n = +attribute.getAttribute("number");
-  console.log(n + "hellooooooooooo n");
-  for (let i = 0; i < el.length; i++) {
-    let distance = elementDistanceFromBottomOfViewport(el[i]);
-    el[i].style.transform = "translateX(-" + distance * n + "px)";
-  }
-}
-
-if (window.addEventListener) {
-  addEventListener('DOMContentLoaded', parallaxTest, false);
-  addEventListener('load', parallaxTest, false);
-  addEventListener('scroll', parallaxTest("attribute"), false);
-} */
-
-// parallaxLeft
-function parallaxLeft() {
-  let el = document.querySelectorAll(".parallaxLeft");
-  let mult = 0.3;
-  for (let i = 0; i < el.length; i++) {
-    let distance = elementDistanceFromBottomOfViewport(el[i]);
-    el[i].style.transform = "translateX(-" + distance * mult + "px)";
-  }
-}
-
-if (window.addEventListener) {
-  addEventListener('DOMContentLoaded', parallaxLeft, false);
-  addEventListener('load', parallaxLeft, false);
-  addEventListener('scroll', parallaxLeft, false);
-}
-
-// parallaxRight
-function parallaxRight() {
-  let el = document.querySelectorAll(".parallaxRight");
-  let mult = 0.5;
-  for (let i = 0; i < el.length; i++) {
-    let distance = elementDistanceFromBottomOfViewport(el[i]);
-    el[i].style.transform = "translateX(" + distance * mult + "px)";
-  }
-}
-
-if (window.addEventListener) {
-  addEventListener('DOMContentLoaded', parallaxRight, false);
-  addEventListener('load', parallaxRight, false);
-  addEventListener('scroll', parallaxRight, false);
-}
-
-// parallaxDown
-function translate(element, n, direction) {
-  let distance = elementDistanceFromBottomOfViewport(element);
-  let multiplier = n;
-
-  element.style.transform = "translateY(" + direction + distance * multiplier + "px)";
-}
-
-function parallaxLeaf() {
-  let leaf1 = document.querySelector(".leaf-box-1");
-  let leaf2 = document.querySelector(".leaf-box-2");
-  let leaf3 = document.querySelector(".leaf-box-3");
-  let leaf4 = document.querySelector(".leaf-box-4");
-  let leaf5 = document.querySelector(".leaf-box-5");
-  let leaf6 = document.querySelector(".leaf-box-6");
-  let leaf7 = document.querySelector(".leaf-box-7");
-  let leaf8 = document.querySelector(".leaf-box-8");
-
-  translate(leaf1, 0.2, "-");
-  translate(leaf2, 0.5, "");
-  translate(leaf3, 0.3, "-");
-  translate(leaf4, 0.2, "");
-  translate(leaf5, 0.2, "");
-  translate(leaf6, 0.1, "-");
-  translate(leaf7, 0.7, "");
-  translate(leaf8, 0.4, "-");
-}
-
-function parallax() {
-  var box = document.querySelector(".about-foreground ");
-
-  var multiplier = 0.2;
-
-  var distance = elementDistanceFromBottomOfViewport(box);
-
-  box.style.transform = "translateY(" + distance * multiplier + "px)";
-
-  // var boxes = document.querySelectorAll(".about-text");
-
-  // var multiplier = 0.2;
-
-  // boxes.forEach(function(box) {
-  //   var distance = elementDistanceFromBottomOfViewport(box);
-  //   box.style.transform = "translateY(" + distance * multiplier + "px)";
-  // });
-}
-
-function setBackgroundHeight() {
-  var bg = document.querySelector(".about-background");
-  var boxes = document.querySelectorAll(".about-foreground");
-  var lastBox = boxes.item(boxes.length - 1);
-
-  // boxes.forEach(function(box) {
-  //   console.log(box.offsetTop + box.offsetHeight);
-  //   console.log(box.getBoundingClientRect());
-  // });
-
-  bg.style.height = lastBox.offsetTop + lastBox.offsetHeight + "px";
 }
